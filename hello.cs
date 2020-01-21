99813ae4b8edb3877c18b4a597bb3878f9305e0b
shakib04 <51458806+shakib04@users.noreply.github.com>
2020-01-21 17:02:57 +0600

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20, c = 30, d;
+            string str = "CS";
+            Console.WriteLine("helllo world");
+            Console.Write("spring\n");
+            //Console.WriteLine("{0},{3},{1}", a,b,c,str);
+            Console.WriteLine("Total student = {0}", d);
+
+            //data types of c#
+
+        }
+    }
+}
